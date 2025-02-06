4ad2190768d693d92f35ca18f314f533892459aa
Bastian CRUVELLIER <b.cruvellier@mac.home>
2025-02-06 12:06:22 +0100

    Portfolio React

---
+++ Portfolio/src/_components/_icons/LyceePng.jsx
@@ -0,0 +1,7 @@
+import Lycee from "../../assets/lycee.png";
+
+function LyceePng() {
+  return <img width="20px" src={Lycee} />;
+}
+
+export default LyceePng;
---
+++ Portfolio/src/_components/_icons/OrtecPng.jsx
@@ -0,0 +1,7 @@
+import Ortec from "../../assets/ortec.png";
+
+function OrtecPng() {
+  return <img width="40px" src={Ortec} />;
+}
+
+export default OrtecPng;
