185185c1f23f334d571914718472463897cc1890
Bastian CRUVELLIER <b.cruvellier@mac.home>
2025-02-06 12:03:58 +0100

    Portfolio React

--- Portfolio/src/_components/_icons/EpitechPng.jsx
+++
@@ -1,5 +0,0 @@
-function EpitechPng() {
-  return <img width="40px" src="/epitech.png" />;
-}
-
-export default EpitechPng;
--- Portfolio/src/_components/_icons/LyceePng.jsx
+++
@@ -1,5 +0,0 @@
-function LyceePng() {
-  return <img width="20px" src="/lycee.png" />;
-}
-
-export default LyceePng;
