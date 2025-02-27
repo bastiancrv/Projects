647da49100d57ec09bbb6970f42a4d508a453185
Bastian CRUVELLIER <b.cruvellier@mac.home>
2025-02-27 17:39:45 +0100

    IRC + Modifs Portfolio

--- IRC/client/src/components/MsgReceived.jsx
+++
@@ -1,9 +0,0 @@
-function MsgReceived({ value, onDelete, deleted }) {
-  return (
-    <div className="group bg-stone-300 text-stone-800 p-4 rounded-xl shadow text-justify w-fit h-fit">
-      <span>{value}</span>
-    </div>
-  );
-}
-
-export default MsgReceived;
